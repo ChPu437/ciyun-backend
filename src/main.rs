2786ca8f9e2c8317c851eb86341dfcd94607d386
Jing Pu <chpu437@163.com>
2023-09-02 21:03:11 +0800

    Split request handler and added qoute_id sending ability for get_qoute handler

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,12 @@ TODO:
 */
 
 use futures_util::lock::Mutex;
+use futures_util::stream::SplitStream;
 use std::sync::Arc;
 use std::{io::Error as IoError, net::SocketAddr};
+use tokio_tungstenite::WebSocketStream;
 
-use futures_util::{SinkExt, StreamExt};
+use futures_util::{stream::SplitSink, SinkExt, StreamExt};
 use tokio::net::{TcpListener, TcpStream};
 use tungstenite::Message;
 
@@ -26,8 +28,6 @@ const POSTGRES_USERNAME: &str = "postgres";
 const POSTGRES_PASSWORD: &str = "fillme";
 const POSTGRES_HOST: &str = "localhost";
 const POSTGRES_DBNAME: &str = "ciyun";
-
-// static SAMPLE_QOUTES: [&str; 5] = ["111", "222", "333", "444", "555"];
 
 #[tokio::main]
 async fn main() -> Result<(), IoError> {
@@ -84,9 +84,31 @@ async fn handler_connection(
 
     let msg = msg_recv.next().await.unwrap().unwrap().to_string();
     println!("[INFO] Received a message from {}: {}", addr, msg);
-    // TODO: use match exp to deal with variable request
+
+    // ~~when using "for_each", we need to manually close the connection~~
+    // ~~or to use one shot instead?~~
+    // NO using one_shot, for we need to recv/send multiple message like passwd
 
     // // To get a qoute
+    // TODO: Add some timeout on connection/request to avoid blockage
+    match msg.as_str() {
+        "fetch_qoute" => handler_req_get_qoute(&mut msg_send, &addr, &pool).await,
+        "write_qoute" => handler_req_write_qoute(&mut msg_send, &mut msg_recv, &addr, &pool).await,
+        "login" => handler_req_login(&mut msg_send, &mut msg_recv, &addr, &pool).await,
+        // We will need a data structure to save login token and state when the server is up
+        // token is generated every login, thus avoid ip change and abuse
+        // Or we save the token directly to the database and refresh them once in a period
+        "like" => handler_req_like(&mut msg_send, &mut msg_recv, &addr, &pool).await,
+        // TODO: We also need to send qoute id to the client as the like function needs id
+        _ => println!("[WARN] Unrecognized message \"{}\"", msg),
+    }
+}
+
+async fn handler_req_get_qoute(
+    stream_send: &mut SplitSink<WebSocketStream<TcpStream>, Message>,
+    addr: &SocketAddr,
+    pool: &Arc<Mutex<sqlx::Pool<Postgres>>>,
+) {
     let pool = pool.lock().await;
     let qoute_count = sqlx::query("SELECT COUNT(*) FROM qoute;")
         .fetch_one(&*pool)
@@ -98,27 +120,52 @@ async fn handler_connection(
 
     // ThreadRng cannot be send through threads safely, thus we use OsRng instead.
     let mut rand = rand::rngs::OsRng::default();
-    let msg = String::from(
-        sqlx::query(
-            format!(
-                "SELECT qoute_text FROM qoute WHERE qoute_id = {}",
-                rand.gen_range(1..qoute_count)
-            )
-            .as_str(),
-        )
-        .fetch_one(&*pool)
-        .await
-        .unwrap()
-        .try_get::<&str, _>("qoute_text")
-        .unwrap(),
+    let qoute_id = rand.gen_range(1..qoute_count);
+    let qoute_text = String::from(
+        sqlx::query(format!("SELECT qoute_text FROM qoute WHERE qoute_id = {}", qoute_id).as_str())
+            .fetch_one(&*pool)
+            .await
+            .unwrap()
+            .try_get::<&str, _>("qoute_text")
+            .unwrap(),
     );
     drop(pool); // remember to unlock the Mutex
 
-    msg_send.send(Message::Text(msg.clone())).await.unwrap();
-    println!("Send message to {}: {}", addr, msg);
+    stream_send
+        .send(Message::Text(qoute_id.to_string()))
+        .await
+        .unwrap();
+    println!("Send message to {}: {}", addr, qoute_id);
 
-    // when using "for_each", we need to manually close the connection
-    // or to use one shot instead?
+    stream_send
+        .send(Message::Text(qoute_text.clone()))
+        .await
+        .unwrap();
+    println!("Send message to {}: {}", addr, qoute_text);
 
     println!("{} disconnected", &addr);
+}
+
+async fn handler_req_write_qoute(
+    stream_send: &mut SplitSink<WebSocketStream<TcpStream>, Message>,
+    stream_recv: &mut SplitStream<WebSocketStream<TcpStream>>,
+    addr: &SocketAddr,
+    pool: &Arc<Mutex<sqlx::Pool<Postgres>>>,
+) {
+}
+
+async fn handler_req_login(
+    stream_send: &mut SplitSink<WebSocketStream<TcpStream>, Message>,
+    stream_recv: &mut SplitStream<WebSocketStream<TcpStream>>,
+    addr: &SocketAddr,
+    pool: &Arc<Mutex<sqlx::Pool<Postgres>>>,
+) {
+}
+
+async fn handler_req_like(
+    stream_send: &mut SplitSink<WebSocketStream<TcpStream>, Message>,
+    stream_recv: &mut SplitStream<WebSocketStream<TcpStream>>,
+    addr: &SocketAddr,
+    pool: &Arc<Mutex<sqlx::Pool<Postgres>>>,
+) {
 }
